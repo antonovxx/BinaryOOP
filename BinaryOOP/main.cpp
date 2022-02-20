603161a47db9ebc8c4c3d9c6adda9a7df61e23c5
antonkurin <antonkurin1@gmail.com>
2022-02-20 09:03:54 +0300

    Initial Commit

---
+++ BinaryOOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BinaryOOP
+//
+//  Created by Антон Курин on 20.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
